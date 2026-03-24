2a76e67c3463e214b877abdc0aacc4ec5fb37847
vhunkaloo <v.hunkalo@webitel.com>
2026-03-24 11:52:53 +0200

    fix(windows): implement single instance lock using mutex

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -7,6 +7,23 @@
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
+  
+  // 1. Single Instance Check using Mutex
+  const wchar_t* mutex_name = L"Global\\Webitel_DeskTrack_Unique_Mutex";
+  HANDLE hMutex = CreateMutexW(NULL, TRUE, mutex_name);
+
+  if (GetLastError() == ERROR_ALREADY_EXISTS) {
+    // Attempt to find the existing window and bring it to front
+    HWND hwnd = FindWindowW(L"FLUTTER_RUNNER_WIN32_WINDOW", L"Webitel DeskTrack");
+    if (hwnd) {
+      ShowWindow(hwnd, SW_RESTORE);
+      SetForegroundWindow(hwnd);
+    }
+    
+    if (hMutex) CloseHandle(hMutex);
+    return EXIT_SUCCESS; // Exit early
+  }
+
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
@@ -27,7 +44,12 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
+  
   if (!window.Create(L"Webitel DeskTrack", origin, size)) {
+    if (hMutex) {
+      ReleaseMutex(hMutex);
+      CloseHandle(hMutex);
+    }
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
@@ -36,6 +58,12 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   while (::GetMessage(&msg, nullptr, 0, 0)) {
     ::TranslateMessage(&msg);
     ::DispatchMessage(&msg);
+  }
+
+  // Cleanup before exit
+  if (hMutex) {
+    ReleaseMutex(hMutex);
+    CloseHandle(hMutex);
   }
 
   ::CoUninitialize();
